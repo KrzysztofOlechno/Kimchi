6b572d57e61cb6bbac3d1e68f90417a8277dc49a
KO <krolechno@wp.pl>
2021-05-30 15:54:19 +0200

    Erfania UPDATE

--- js/script.js
+++ js/script.js
@@ -1,65 +1,111 @@
-startButton = document.getElementById("startButton");
-
-button1k3 = document.getElementById("button1k3");
-button1k4 = document.getElementById("button1k4");
-button1k6 = document.getElementById("button1k6");
-button1k8 = document.getElementById("button1k8");
-button1k10 = document.getElementById("button1k10");
-button1k12 = document.getElementById("button1k12");
-button1k20 = document.getElementById("button1k20");
-
+buttonThrow = document.getElementsByClassName("buttonThrow")
 
 // shake
 
-button1k3.addEventListener('click', function(){
-    diceThrow(3);
-});
+for(var i=0;i<6;i++)
+{
+	buttonThrow[i].addEventListener('click', function(){
+    getPlayerData(document.baseURI, this.id);	
+	});
+}
 
-button1k4.addEventListener('click', function(){
-    diceThrow(4);
-});
+function getPlayerData(playerURI, skillId)
+{
+let requestURL = 'json/players.json';
+let request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) 
+  {
+    var myObj = JSON.parse(this.responseText);    
+    for(var i=0;i<myObj.length;i++)
+    {
+      if(playerURI.includes(myObj[i].name.toLowerCase()))
+      {
+        var playerSkills = myObj[i].skills;
+        for(var j=0;j<playerSkills.length;j++)
+        {
+          if(playerSkills[j].name == skillId)
+          {
+            var pskill = playerSkills[j];
+            var base = pskill.basevalue;
+            var buff = pskill.buffvalue;
+            var secbuff = pskill.secbuffvalue
 
-button1k6.addEventListener('click', function(){
-    diceThrow(6);
-});
+            throwAndSum(base, buff, secbuff);
+          }
+        }
+      } 
+    }
+  }
+};
+request.open('GET', requestURL);
+request.send();
 
-button1k8.addEventListener('click', function(){
-    diceThrow(8);
-});
+}
 
-button1k10.addEventListener('click', function(){
-    diceThrow(10);
-});
+function throwAndSum(base, second, third)
+{
+  var fir = skillToCubeConverter(base);
+  var sec = skillToCubeConverter(second);
+  var thi = skillToCubeConverter(third);
 
-button1k12.addEventListener('click', function(){
-    diceThrow(12);
-});
+  var sum = 0;
 
-button1k20.addEventListener('click', function(){
-    diceThrow(20);
-});
+  if(fir != null)
+    {
+      console.log(fir);
+      sum += diceThrow(fir);
+      console.log('Sum:' + sum);
+    }
+  if(sec != null)
+    {
+      console.log(sec);
+      sum += diceThrow(sec);
+      console.log('Sum:' + sum);
+    }
+  if(thi != null)
+    {
+      sum += diceThrow(thi);
+      console.log('Sum:' + sum);
+    }
+    console.log('Suma to: ' + sum);
+speakNumber(sum);
+}
+
+function skillToCubeConverter(skillValue)
+{
+  switch(skillValue) {
+    case 1:
+      return 4;
+    case 2:
+      return 6;
+    case 3:
+      return 8;
+    case 4:
+      return 10;
+    case 5:
+      return 12;
+    case 6:
+      return 20;
+    default:
+      return null;
+  }
+}
 
 function diceThrow(cubeType)
 {
-    
+	
     var cube = parseInt((Math.random()*cubeType)+1);
     var cubeTypeString = cubeType.toString();
     console.log('W wyniku rzutu kością 1k' + cubeTypeString + ' wypadła wartość:',cube);
-    speakNumber(cubeType, cube);
-    
+    return cube;
 }
 
-function speakNumber(cubeType, number)
+function speakNumber(number)
 {
-    playSound('soundsystem/shake1.mp3');
-    var delay = 2000;
     var soundUrlString = 'soundsystem/' + number.toString() + '.mp3';
-    
-    setTimeout(function(){
-        playSound(soundUrlString);
-    }, delay);
-    
-    console.log(soundUrlString, cubeType);
+    playSound(soundUrlString);  
+    console.log(soundUrlString);
 }
 
 function playSound(url) {
